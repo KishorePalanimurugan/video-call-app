10e1713f991dfee72d55130e36b27f8a2cb6da00
kishorePalanimurugan <kishore@minidreams.in>
2025-01-09 12:29:01 +0530

    dashboard added

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,8 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
-import {
-  Container,
-} from "reactstrap";
+import { Card, CardBody, CardTitle, Col, Container, Row } from "reactstrap";
 
 //Import Breadcrumb
 import Breadcrumbs from "../../components/Common/Breadcrumb";
@@ -15,17 +13,14 @@ import { useSelector } from "react-redux";
 import { createSelector } from "reselect";
 
 const Dashboard = (props) => {
-
   const DashboardProperties = createSelector(
     (state) => state.Dashboard,
     (dashboard) => ({
-      chartsData: dashboard.chartsData
+      chartsData: dashboard.chartsData,
     })
   );
 
-  const {
-    chartsData
-  } = useSelector(DashboardProperties);
+  const { chartsData } = useSelector(DashboardProperties);
 
   //meta title
   document.title = "Admin Panel";
@@ -34,10 +29,78 @@ const Dashboard = (props) => {
     <React.Fragment>
       <div className="page-content">
         <Container fluid>
-          <Breadcrumbs title={props.t("Dashboards")} breadcrumbItem={props.t("Dashboard")} />
+          <Breadcrumbs
+            title={props.t("Dashboards")}
+            breadcrumbItem={props.t("Dashboard")}
+          />
+          <Row>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Total Boys Count</CardTitle>
+                  1000
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Total Girls Count</CardTitle>
+                  80
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Total Audio Calls</CardTitle>
+                  12k
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Total Video Calls</CardTitle>
+                  32k
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Total Payments</CardTitle>
+                  1,00,000
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Coin Price (per coin)</CardTitle>
+                  20
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Audio Call Price (per minute)</CardTitle>
+                  10 coins
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <CardBody>
+                  <CardTitle>Video Call Price (per minute)</CardTitle>
+                  50 coins
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
         </Container>
       </div>
-
     </React.Fragment>
   );
 };
